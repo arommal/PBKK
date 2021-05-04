78fcacf1e28a1eb53af810712697224154310b52
Rosa Valentine <rosalammora@gmail.com>
2021-05-04 12:39:34 +0700

    Add project folders

---
+++ LatihanMVVM/ItemPenjualan.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMVVM
+{
+    class ItemPenjualan
+    {
+        public ItemPenjualan()
+        {
+            DiskonPersen = 0;
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [StringLength(50)]
+        public string NamaBarang { get; set; }
+
+        public int Jumlah { get; set; }
+
+        public decimal Harga { get; set; }
+
+        public decimal DiskonPersen { get; set; }
+
+        public decimal Total()
+        {
+            decimal total = Jumlah * Harga;
+            return total - (DiskonPersen / 100 * total);
+        }
+    }
+}
---
+++ LatihanMVVM/LatihanContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMVVM
+{
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+    class LatihanContext : DbContext
+    {
+        public DbSet<ItemPenjualan> DaftarItemPenjualan { get; set; }
+    }
+}
